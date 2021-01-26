b22ce9bd9b757d291efbcd824bc0022afd70f86b
James Kwok <james.z.kwok@gmail.com>
2021-01-26 14:22:28 +1100

    working with rest api

--- src/hello-service/main.go
+++ src/hello-service/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"encoding/json"
 )
+
+type Greeting struct {
+    Greeting string
+}
 
 func main() {
 	handler := http.NewServeMux()
 	handler.HandleFunc("/api/hello", SayHelloMethod)
-	http.ListenAndServe("0.0.0.0:8080", handler)
+	http.ListenAndServe("0.0.0.0:3000", handler)
 }
 
 func SayHelloMethod(w http.ResponseWriter, r *http.Request) {
+	data := Greeting{Greeting: "Hello World!"}
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, `Hello world`)
+    w.WriteHeader(http.StatusOK)
+    json.NewEncoder(w).Encode(data)
 }
